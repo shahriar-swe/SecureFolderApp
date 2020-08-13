5588c7cb1315b7d53939377582b86199c4e61f46
shahriar-swe <shahriarswe@gmail.com>
2020-08-13 11:35:39 -0700

    8th commit by Shahriar Momen "PDF Upload & View Font End Design Code + Back End Code"

---
+++ app/src/main/java/com/example/securefolder/uploadPDF.java
@@ -0,0 +1,29 @@
+package com.example.securefolder;
+
+public class uploadPDF {
+
+    public String name;
+    public String url;
+
+
+    public uploadPDF() {
+    }
+
+    public uploadPDF(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public String getUrl() {
+        return url;
+    }
+
+
+}
